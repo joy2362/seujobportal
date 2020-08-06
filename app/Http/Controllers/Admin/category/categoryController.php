06a3b1f7611693045c10b0ca82288541cae7b2f7
joy2362 <abdullahzahidjoy@gmail.com>
2020-08-06 21:40:08 +0600

    asdf

--- app/Http/Controllers/Admin/category/categoryController.php
+++ app/Http/Controllers/Admin/category/categoryController.php
@@ -59,7 +59,7 @@ class categoryController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
